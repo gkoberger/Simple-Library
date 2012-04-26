f85a3c6c1eaaaaa1f93e6199b3399fdd902f042a
Gregory Koberger <gkoberger@gmail.com>
2012-04-25 20:53:55 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var u = new User();
+
+function getName() {
+    return u.name;
+}
+
+function setName( name ) {
+    u.name = name;
+}
+
+function getAge() {
+    return u.name;
+}
+
+function setAge( age ) {
+    u.age = age;
+}
